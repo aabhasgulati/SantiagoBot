4d13adc27a2ca0242d8036d5c2ee6075fcee278f
Aabhas Gulati <imaabhasgulati@gmail.com>
2020-12-02 14:38:57 +0530

    updates

--- santiagobot.py
+++ santiagobot.py
@@ -4,9 +4,7 @@ from discord.utils import get
 import asyncio
 
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
